8d269b463248375932787fddcef9fccf0ba2c24d
adeka stanley <adekastanley2@gmail.com>
2026-01-28 02:24:42 +0100

    logo fix

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,7 +2,7 @@ export const Logo = () => {
 	return (
 		<div className="flex items-center gap-2 font-bold select-none text-xl">
 			<img
-				src="/sandef-logo.svg"
+				src="/Sandef-logo.svg"
 				width={100}
 				height={100}
 				className="drop-shadow-[0_0_1px_rgba(255,255,255,0.4)]"
